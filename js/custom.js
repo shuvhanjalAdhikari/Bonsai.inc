83d8c6647d2fd487dfa62ea2291dceaaf1564c9a
shuvhanjalAdhikari <135438079+shuvhanjalAdhikari@users.noreply.github.com>
2024-08-01 14:40:32 +0545

    first commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+function myFunction() {
+  var x = document.getElementById("popup");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function textmore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "View More"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
